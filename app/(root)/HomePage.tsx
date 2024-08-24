177e05f3bd201f8762b1e394d3aebdc275dae008
KrishanMihiranga <xp3cto95@gmail.com>
2024-08-24 06:49:05 +0530

    feat: Implement Home Page

--- app/(root)/HomePage.tsx
+++ app/(root)/HomePage.tsx
@@ -4,15 +4,18 @@ import DashboardButton from "../components/DashboardButton";
 
 const Home = () => {
   return (
-    <section id="home">
-      <div>
-        <Image src={background}
+    <section id="home" className="relative">
+      <div className="bg-black absolute w-full h-full z-10 opacity-45"></div>
+      <div className="relative flex flex-col items-start justify-center">
+        <Image
+          src={background}
           alt="Tea leaves background image for LCC Tea Collectors"
+          className="w-full max-h-[850px] object-cover"
         />
-        <div>
-          <header>
-            <h1>LCC Tea Collectors</h1>
-            <p className="text-lg font-semibold">A legacy of quality since 1980</p>
+        <div className="absolute z-10 px-32">
+          <header className="text-white">
+            <h1 className="text-9xl">LCC Tea  <br /> Collectors</h1>
+            <p className="text-lg">A legacy of quality since 1980</p>
           </header>
           <DashboardButton />
         </div>
--- app/components/DashboardButton.tsx
+++ app/components/DashboardButton.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+"use client";
 
 const DashboardButton = () => {
-  return (
-    <button>Contact Us</button>
-  )
-}
+  const scrollToContacts = () => {
+    const element = document.getElementById('contact');
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
 
-export default DashboardButton
+  return (
+    <button
+      className='bg-[#FFA200] w-[200px] h-[50px] rounded-lg mt-3 text-white text-lg cursor-pointer hover:bg-[#FFA200]/80 duration-300'
+      onClick={scrollToContacts}
+    >
+      Contact Us
+    </button>
+  );
+};
+
+export default DashboardButton;
